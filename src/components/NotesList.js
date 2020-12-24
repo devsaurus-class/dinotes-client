110f2a1602b2f10c3eef6ab87cb3683d8a739bf5
yogiokto <yogiokto@outlook.com>
2020-12-24 16:12:00 +0700

    add search feature

--- src/components/NotesList.js
+++ src/components/NotesList.js
@@ -1,21 +1,20 @@
 /* eslint-disable no-underscore-dangle */
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import tw from 'twin.macro';
 import { Link } from 'react-router-dom';
 import { useSelector, useDispatch } from 'react-redux';
-
-import { getAllNotes, fetchNotes } from '../features/notes/notesSlice';
+import { fetchNotes, getFilteredNotes } from '../features/notes/notesSlice';
+import Container from './ui/Container';
 
 const NotesListContainer = tw.div`grid grid-cols-1 md:grid-cols-3 gap-4 my-8`;
-
-const Card = tw.div`text-left p-4 border rounded-md`
-
+const Card = tw.div`text-left p-4 border rounded-md`;
 const Title = tw.h4`text-lg font-semibold text-purple-900`;
-
+const SearchBar = tw.input`my-4 p-2 text-left border rounded focus:outline-none focus:ring focus:border-blue-300`;
 
 const NotesList = () => {
+  const [keyword, setKeyword] = useState('');
   const dispatch = useDispatch();
-  const notes = useSelector(getAllNotes);
+  const notes = useSelector((state) => getFilteredNotes(state, keyword));
   const notesStatus = useSelector((state) => state.notes.status);
   const error = useSelector((state) => state.notes.error);
 
@@ -24,6 +23,10 @@ const NotesList = () => {
       dispatch(fetchNotes());
     }
   }, [notesStatus, dispatch]);
+
+  const handleChange = (e) => {
+    setKeyword(e.target.value);
+  };
 
   let content;
 
@@ -44,7 +47,12 @@ const NotesList = () => {
     content = <div>{error}</div>;
   }
 
-  return <NotesListContainer>{content}</NotesListContainer>;
+  return (
+    <Container>
+      <SearchBar placeholder="Search Notes..." onChange={handleChange} />
+      <NotesListContainer>{content}</NotesListContainer>
+    </Container>
+  );
 };
 
 export default NotesList;
--- src/features/notes/notesSlice.js
+++ src/features/notes/notesSlice.js
@@ -1,137 +1,132 @@
-import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
+import { createSlice, createAsyncThunk } from '@reduxjs/toolkit';
 
 const initialState = {
   data: [],
-  status: "idle",
-  error: null,
+  status: 'idle',
+  error: null
 };
 
-export const fetchNotes = createAsyncThunk("notes/fetchNotes", async () => {
+export const fetchNotes = createAsyncThunk('notes/fetchNotes', async () => {
   const response = await fetch(`${process.env.REACT_APP_API_URL}/notes`);
   const data = await response.json();
   return data;
 });
 
-export const addNewNote = createAsyncThunk(
-  "notes/AddNewNote",
-  async (initialNotes) => {
-    const requestOptions = {
-      method: "POST",
-      headers: { "Content-Type": "application/json" },
-      body: JSON.stringify(initialNotes),
-    };
+export const addNewNote = createAsyncThunk('notes/AddNewNote', async (initialNotes) => {
+  const requestOptions = {
+    method: 'POST',
+    headers: { 'Content-Type': 'application/json' },
+    body: JSON.stringify(initialNotes)
+  };
 
-    const response = await fetch(
-      `${process.env.REACT_APP_API_URL}/note`,
-      requestOptions
-    );
-    if (response.ok) {
-      const data = await response.json();
-      const noteAdded = { ...initialNotes, _id: data._id };
-      return noteAdded;
-    } else {
-      return null;
-    }
+  const response = await fetch(`${process.env.REACT_APP_API_URL}/note`, requestOptions);
+  if (response.ok) {
+    const data = await response.json();
+    const noteAdded = { ...initialNotes, _id: data._id };
+    return noteAdded;
+  } else {
+    return null;
   }
-);
+});
 
-export const updateExistingNote = createAsyncThunk(
-  "notes/updateNote",
-  async(currentNote) => {
-    const requestOptions = {
-      method: 'PUT',
-      headers: { 'Content-Type': 'application/json' },
-      body: JSON.stringify(currentNote)
-    };
+export const updateExistingNote = createAsyncThunk('notes/updateNote', async (currentNote) => {
+  const requestOptions = {
+    method: 'PUT',
+    headers: { 'Content-Type': 'application/json' },
+    body: JSON.stringify(currentNote)
+  };
 
-    const response = await fetch(`${process.env.REACT_APP_API_URL}/note/${currentNote._id}`, requestOptions);
-    if (response.ok) {
-      return currentNote;
-    } else {
-      return null;
-    }
+  const response = await fetch(`${process.env.REACT_APP_API_URL}/note/${currentNote._id}`, requestOptions);
+  if (response.ok) {
+    return currentNote;
+  } else {
+    return null;
   }
-);
+});
 
-export const deleteNote = createAsyncThunk(
-  "notes/deleteNote",
-  async(currentNote) => {
-    const requestOptions = {
-      method: 'DELETE',
-      headers: { 'Content-Type': 'application/json' }
-    };
+export const deleteNote = createAsyncThunk('notes/deleteNote', async (currentNote) => {
+  const requestOptions = {
+    method: 'DELETE',
+    headers: { 'Content-Type': 'application/json' }
+  };
 
-    const response = await fetch(`${process.env.REACT_APP_API_URL}/note/${currentNote._id}`, requestOptions);
-    if (response.ok) {
-      return currentNote;
-    } 
+  const response = await fetch(`${process.env.REACT_APP_API_URL}/note/${currentNote._id}`, requestOptions);
+  if (response.ok) {
+    return currentNote;
   }
-)
-
+});
 
 const notesSlice = createSlice({
-  name: "notes",
+  name: 'notes',
   initialState,
   reducers: {
     statusReset(state, action) {
-      state.status = "idle";
-    },
+      state.status = 'idle';
+    }
   },
   extraReducers: {
     [fetchNotes.pending]: (state, action) => {
-      state.status = "loading";
+      state.status = 'loading';
     },
     [fetchNotes.fulfilled]: (state, action) => {
-      state.status = "succeeded";
+      state.status = 'succeeded';
       state.data = action.payload;
     },
     [fetchNotes.rejected]: (state, action) => {
-      state.status = "failed";
+      state.status = 'failed';
       state.error = action.error.message;
     },
     [addNewNote.pending]: (state, action) => {
-      state.status = "loading";
+      state.status = 'loading';
     },
     [addNewNote.fulfilled]: (state, action) => {
-      state.status = "succeeded";
+      state.status = 'succeeded';
       state.data.push(action.payload);
     },
     [addNewNote.rejected]: (state, action) => {
-      state.status = "failed";
+      state.status = 'failed';
       state.error = action.error.message;
     },
     [updateExistingNote.pending]: (state, action) => {
-      state.status = "loading";
+      state.status = 'loading';
     },
     [updateExistingNote.fulfilled]: (state, action) => {
-      state.status = "succeeded";
+      state.status = 'succeeded';
       const { _id, title, note } = action.payload;
-      const existingNote = state.data.find(note => note._id === _id);
+      const existingNote = state.data.find((note) => note._id === _id);
       if (existingNote) {
         existingNote.title = title;
         existingNote.note = note;
       }
     },
     [updateExistingNote.rejected]: (state, action) => {
-      state.status = "failed";
+      state.status = 'failed';
       state.error = action.error.message;
     },
     [deleteNote.pending]: (state, action) => {
-      state.status = "loading";
+      state.status = 'loading';
     },
     [deleteNote.fulfilled]: (state, action) => {
-      state.status = "succeeded";
+      state.status = 'succeeded';
       const { _id } = action.payload;
-      const updatedNotes = state.data.filter(note => note._id === _id);
+      const updatedNotes = state.data.filter((note) => note._id === _id);
       state.data = updatedNotes;
     },
     [deleteNote.rejected]: (state, action) => {
-      state.status = "failed";
+      state.status = 'failed';
       state.error = action.error.message;
-    },
-  },
+    }
+  }
 });
 export const getAllNotes = (state) => state.notes.data;
+export const getFilteredNotes = (state, keyword) => {
+  if (keyword) {
+    const isKeywordExist = (array, string) => array.toLowerCase().includes(string);
+
+    return state.notes.data.filter(note => isKeywordExist(note.note, keyword) || isKeywordExist(note.title, keyword));
+  }
+  return state.notes.data;
+};
 export const getNoteById = (state, noteId) => state.notes.data.find((note) => note._id === noteId);
 
 export const { statusReset } = notesSlice.actions;
